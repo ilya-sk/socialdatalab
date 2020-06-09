9ff4caa3fb88eb089acfd3bb7802aa07cea588ca
ilya-sk <ilyarus@inbox.ru>
2020-06-10 00:21:31 +0300

    without slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,22 @@
-$(function(){
+// $(function(){
 
-    $('.slider-content').slick();  
+//     $('.slider-content').slick({
+//         vertical: true,
+//   verticalSwiping: true,
+//   arrows: false,
+//   infinite: true,
+//   dots: true,
+//     });  
+// });
+$(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+        scrollHorizontally: true,
+  
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
 });
+
